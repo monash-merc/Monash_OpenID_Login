ea6df582c236457396b973ad345456fe9c492487
manishkumr <rishimanish123@gmail.com>
2018-07-26 15:08:10 +1000

    reverting this change until #1195 is fixed

--- views.py
+++ views.py
@@ -28,7 +28,7 @@ class LoginView(TemplateView):
         if request.user.is_authenticated:
             # redirect the user to the home page if he is trying to go to the
             # login page
-            return HttpResponseRedirect(request.GET.get('next', '/'))
+            return HttpResponseRedirect(request.GET.get('next_page', '/'))
 
         c = self.get_context_data(request, **kwargs)
         return render_response_index(request, self.template_name, c)
@@ -39,7 +39,7 @@ class LoginView(TemplateView):
         if request.user.is_authenticated:
             # redirect the user to the home page if he is trying to go to the
             # login page
-            return HttpResponseRedirect(request.POST.get('next', '/'))
+            return HttpResponseRedirect(request.POST.get('next_page', '/'))
 
         # TODO: put me in SETTINGS
         if 'username' in request.POST and \
