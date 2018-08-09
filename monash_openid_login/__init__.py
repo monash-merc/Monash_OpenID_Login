6663026a3ca1a2d8350d794a6f8afbea5363405e
James Wettenhall <James.Wettenhall@monash.edu>
2018-08-09 11:55:56 +1000

    Adding an AbstractTardisAppConfig to ensure that MyTardis will find this app's urls.py

---
+++ monash_openid_login/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'monash_openid_login.apps.MonashAppConfig'
---
+++ monash_openid_login/apps.py
@@ -0,0 +1,6 @@
+from tardis.app_config import AbstractTardisAppConfig
+
+
+class MonashAppConfig(AbstractTardisAppConfig):
+    name = 'monash_openid_login'
+    verbose_name = 'Monash OpenID Login'
