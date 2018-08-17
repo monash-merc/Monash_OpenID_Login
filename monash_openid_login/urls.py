3045a93398977a5fbf631da0c3144d4e0551b02a
James Wettenhall <James.Wettenhall@monash.edu>
2018-08-17 20:14:48 +1000

    Removed check_account_migration from urls.py

--- monash_openid_login/urls.py
+++ monash_openid_login/urls.py
@@ -6,6 +6,4 @@ from . import views
 urlpatterns = [
     url(r'^login/$', views.LoginView.as_view(),
         name='tardis.apps.monash.views.migrate_accounts'),
-    url(r'^check-account-migration/$', views.check_account_migration,
-        name='tardis.apps.monash.views.check_account_migration')
 ]
