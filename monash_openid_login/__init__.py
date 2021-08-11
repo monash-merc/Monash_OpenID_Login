3c6cff2a4a6bef4a28889d989c9e676be055dff6
Dmitry Yakhnov <dmitry@yakhnov.info>
2021-08-11 21:49:42 +1000

    Bootstrap v5

--- monash_openid_login/__init__.py
+++
@@ -1,1 +0,0 @@
-default_app_config = 'monash_openid_login.apps.MonashAppConfig'
--- monash_openid_login/apps.py
+++ monash_openid_login/apps.py
@@ -2,5 +2,5 @@ from tardis.app_config import AbstractTardisAppConfig
 
 
 class MonashAppConfig(AbstractTardisAppConfig):
-    name = 'monash_openid_login'
+    name = 'tardis.apps.monash_openid_login'
     verbose_name = 'Monash OpenID Login'
